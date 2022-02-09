04d7ab2acea0f7f51e1a5e4e252dfb7c13e25bed
aklusa <aklusa71@gmail.com>
2022-02-09 01:52:33 -0600

    Update

--- app/screens/TabThreeScreen.tsx
+++ app/screens/TabThreeScreen.tsx
@@ -3,15 +3,43 @@ import axios from 'axios';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import {useEffect, useState} from "react";
 
-export default function TabThreeScreen({ navigation }: RootTabScreenProps<'TabThree'>) {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Tab Three</Text>
-            <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-            <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-        </View>
-    );
+export default function TabThreeScreen({ navigation }: RootTabScreenProps<'TabThree'>)  {
+    const [users, setUsers] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        axios.get('http://18.222.116.189:8000/api/users')
+            .then(response => {
+                setUsers(response.data)
+                setIsLoaded(true);
+            })
+            .catch(err => console.log(err))
+
+    }, [])
+
+    if(!isLoaded) {
+        return (
+            <Text style={styles.title}>Loading Data!</Text>
+        )
+    } else {
+
+        return (
+
+
+            <View style={styles.container}>
+                <Text style={styles.title}>Users:</Text>
+                {users.map((user, index) => {
+                    return(
+                        <Text style={styles.title}>{user.firstName} {user.lastName}</Text>
+                    )
+
+                })}
+            </View>
+        );
+    }
+
 }
 
 
